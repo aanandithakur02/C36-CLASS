fb22ae63c1f1d8ee3ebdac22cae2b172d09f09b2
aanandithakur02 <72301057+aanandithakur02@users.noreply.github.com>
2021-02-22 17:16:20 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,47 +1,61 @@
-var canvas, backgroundImage;
+//Create variables here
+var Dog,HappyDog,Dog1,database,foodS,foodStock;
 
-var gameState = 0;
-var playerCount;
-var allPlayers;
-var distance = 0;
-var database;
-
-var form, player, game;
-
-var cars, car1, car2, car3, car4;
-var carImage1,carImage2,carImage3,carImage4;
-var track;
-
-
-function preload(){
-  carImage1 = loadImage("images/car1.png")
-  carImage2 = loadImage("images/car2.png")
-  carImage3 = loadImage("images/car3.png")
-  carImage4 = loadImage("images/car4.png")
-  track = loadImage("images/track.jpg")
+function preload()
+{
+  //load images here
+  Dog = loadImage("images/dogImg.png");
+  HappyDog = loadImage("images/dogImg1.png");
 }
 
-
-function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+function setup() {
+  createCanvas(700 , 650);
+  
+  Dog1 = createSprite(350,500,20,20);
+  Dog1.addImage("normal",Dog);
+  Dog1.addImage("happy",HappyDog);
+  Dog1.scale = 0.4;
   database = firebase.database();
-  game = new Game();
-  game.getState();
-  game.start();
+  foodStock = database.ref("food");
+  foodStock.on("value",readStock);
+
 }
 
 
-function draw(){
-  if(playerCount === 4){
-    game.update(1);
+function draw() {  
+  background(46,139,87);
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    Dog1.changeImage("happy");
   }
-  if(gameState === 1){
-    clear();
-    game.play();
+  if(keyWentUp(UP_ARROW)){
+    Dog1.changeImage("normal");
   }
-  if(gameState === 2){
-    game.end()
-    game.update(2)
-   
-  }
+  drawSprites();
+  //add styles here
+  textSize(20);
+  fill("pink");
+  stroke("white");
+  strokeWeight(2);
+  text("Food : " + foodS,250,250);
+ text("Note : Press UP_ARROW to feed the dog",200,100);
 }
+
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeStock(x){
+  
+  if(x<=0){
+    x = 0;
+  }
+  else{
+    x = x-1;
+  }
+  database.ref("/").update({
+    food :x 
+  })
+}
+
